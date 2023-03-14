ebd8a78d8e5ee1376ca414731a889065f3d55c38
MarcosDMT <marcosgav80@gmail.com>
2023-03-14 22:10:33 +0300

    added a protected route functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import SideBar from "@/components/SideBar";
 import "./globals.css";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/hello/auth/[...nextauth]";
+import Login from "@/components/Login";
 
 export const metadata = {
   title: "ChatGPT Clone",
@@ -21,12 +22,18 @@ export default async function RootLayout({
     <html lang="en">
       <body>
         <SessionProvider session={session}>
+          {/* check if user has a current session available */}
+          {!session ? 
+          <Login />
+          :
           <div className="flex">
             <div className="w-3/12 text-white overflow-y-auto bg-[#202123] p-2">
               <SideBar />
             </div>
             <div className="bg-[#343541] flex-1">{children}</div>
           </div>
+          }
+          
         </SessionProvider>
       </body>
     </html>
